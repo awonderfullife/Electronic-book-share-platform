c655944dfa073e6b1466dc675b9d7b34ac775d1d
cooper.yi <cooper.yi@outlook.com>
2017-05-08 17:58:31 +0800

    clear up

--- project_database/createform.sql
+++
@@ -1,9 +0,0 @@
-use ebookdata
-go
-if exists(select name from sys.tables where name='Student_test')
-drop table Student_test
-go
-create table Student_test
-(sname nchar(10) primary key,
- sex nchar(2) not null,
-)
