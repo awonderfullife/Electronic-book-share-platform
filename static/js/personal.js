9ca3a85a38fb7545d0dd4c1636417e9d4482e5e2
hawnzug <hawnzug@gmail.com>
2017-05-14 17:49:24 +0800

    Improved: update current page after upload success

--- static/js/personal.js
+++ static/js/personal.js
@@ -76,6 +76,52 @@ $("#purchased-li").click(showPurchasedBook);
 $("#favored-li").click(showFavoredBook);
 $("#uploaded-li").click(showUploadBook);
 
+$('#upload-form').on('submit',(function(e) {
+    e.preventDefault();
+    var formData = new FormData(this);
+    var progressBar = $("#progress-bar");
+    $.ajax({
+        xhr: function() {
+            var xhr = new window.XMLHttpRequest();
+            xhr.upload.addEventListener("progress", function(evt) {
+                if (evt.lengthComputable) {
+                    var percentComplete = evt.loaded / evt.total;
+                    percentComplete = parseInt(percentComplete * 100);
+                    progressBar.css('width', percentComplete + '%');
+                    progressBar.html(percentComplete + '%');
+                    if (percentComplete === 100) {
+                        progressBar.html("Success!");
+                    }
+                }
+            }, false);
+            return xhr;
+        },
+        type: 'POST',
+        url: "/api/v1/upload",
+        data: formData,
+        cache: false,
+        contentType: false,
+        processData: false,
+        success: function(data) {
+            console.log("success");
+            console.log(data);
+            if ($("#uploaded-li").hasClass("active")) {
+                showUploadBook();
+            }
+            $("#upload").modal("hide");
+            $('#upload-success').click();
+            $("#upload-form input").val("");
+            $("#upload-form textarea").val("");
+            progressBar.css('width', '0%');
+            progressBar.html('0%');
+        },
+        error: function(data) {
+            console.log("error");
+            console.log(data);
+        }
+    });
+}));
+
 function page_num() {
     html = [
 '<hr>',
