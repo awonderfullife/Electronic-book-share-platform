c551a7831c32a874144b774598b1e0c3114b282b
hawnzug <hawnzug@gmail.com>
2017-06-17 12:20:28 +0800

    Add validator for signup

--- static/js/signup.js
+++ static/js/signup.js
@@ -1,20 +1,22 @@
-$('#signup-form').on('submit', (function(e) {
-    e.preventDefault();
-    var formData = new FormData(this);
-    $.ajax({
-        type: 'POST',
-        url: '/register',
-        data: formData,
-        contentType: false,
-        processData: false,
-        success: function(data) {
-            console.log(data);
-            $('#register-success').click();
-        },
-        error: function(jqXHR, textStatus, errorThrown) {
-            $('#used-email').removeClass('hidden');
-        }
-    });
+$('#signup-form').validator().on('submit', (function(e) {
+    if (!e.isDefaultPrevented()) {
+        e.preventDefault();
+        var formData = new FormData(this);
+        $.ajax({
+            type: 'POST',
+            url: '/register',
+            data: formData,
+            contentType: false,
+            processData: false,
+            success: function(data) {
+                console.log(data);
+                $('#register-success').click();
+            },
+            error: function(jqXHR, textStatus, errorThrown) {
+                $('#used-email').removeClass('hidden');
+            }
+        });
+    }
 }));
 
 $('#back').click((function(e) {
